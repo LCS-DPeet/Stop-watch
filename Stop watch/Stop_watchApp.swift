f0ea9800d48e03ec83a3bd2b78c8a174574ae90d
Danika Peet <DPeet@lcs.on.ca>
2023-11-06 20:59:08 -0500

    Initial Commit

---
+++ Stop watch/Stop_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stop_watchApp.swift
+//  Stop watch
+//
+//  Created by Danika Peet on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Stop_watchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
